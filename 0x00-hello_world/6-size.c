ed55e8363bdc56119a52a25dfdb8796407f55088
Joe-metry <joemetry150@gmail.com>
2022-12-08 13:03:50 -0800

    six size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Entry point
  * Return 1 (Success)
@@ -9,5 +8,4 @@ int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
 	return (1);
-
 }

fdb5b173704d190bfb6bd63a33e04e442b44e7d3
Joe-metry <joemetry150@gmail.com>
2022-12-08 07:54:11 -0800

    six size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,13 +6,12 @@
 
 /**
  *
- *  * main - Entry point
+ *  main - Entry point
  *
- *   *
+ *  
  *
- *    * Return: Always 0 (Success)
- *
- *     */
+ *     Return: Always 0 (Success)
+ * */
 
 
 

6f893838a8bb728e7fbf65c81857badf9166928f
Joe-metry <joemetry150@gmail.com>
2022-12-08 08:39:27 -0800

    five print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,23 +3,15 @@
 
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * Return: Always 0 (success)
  *
- *   *
- *
- *    * Return: Always 0 (success)
- *
- *     */
-
+ */
 int main(void)
-
 {
-
 		printf("with proper grammar, but the outcome is a piece of art,\n");
 
 			return (0);
-
-
 
 }

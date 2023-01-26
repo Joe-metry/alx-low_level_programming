6c570f9e3141341d7b5c4a009d048244ee090f65
Joe-metry <joemetry150@gmail.com>
2023-01-26 23:06:02 +0100

    update on singly linked list task

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,21 @@
 #include "lists.h"
+
 /**
- * list_len - returns the number of elements in a list.
- * @h: singly linked list.
- * Return: number of elements in the list.
+ * list_len - finds the number of elements in a linked list
+ * @h: pointer to the list
+ *
+ *Return: the number of elements
  */
+
 size_t list_len(const list_t *h)
 {
-	size_t count;
+	size_t elements = 0;
 
-	count = 0;
-	while (h != NULL)
+	while (h)
 	{
+		elements++;
 		h = h->next;
-		count++;
 	}
 
-	return (count);
+	return (elements);
 }
